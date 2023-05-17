c30ef49ec240efb4e0117cc92683a031d011628f
karan jain <karanjain2212@gmail.com>
2023-05-17 20:29:09 +0530

    fixed linters

--- morse.rb
+++ morse.rb
@@ -1,26 +1,25 @@
 MORSE_CODE = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-    '--..' => 'Z'
-  }.freeze
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
-  def decode_char(char)
-    MORSE_CODE[char]
-  end
+def decode_char(char)
+  MORSE_CODE[char]
+end
 
-  def decode_word(word)
-    word.split.map { |char| decode_char(char) }.join
-  end
+def decode_word(word)
+  word.split.map { |char| decode_char(char) }.join
+end
 
-  def decode_sentence(sentence)
-    sentence.split('  ').map { |word| decode_word(word) }.join(' ')
-  end
+def decode_sentence(sentence)
+  sentence.split('  ').map { |word| decode_word(word) }.join(' ')
+end
 
- 
-  puts(decode_word("-- -.--"))
 
-  puts decode_sentence(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
- 
+puts(decode_word('-- -.--'))
+
+puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
